1c8c80159daa45f40372d103543f1bde709df28c
Axel-Coelho <66997267+Axel-Coelho@users.noreply.github.com>
2022-06-24 18:59:18 +0200

    Importation Routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const tasks = required("./routes/tasks");
 const connection = require('./db');
 const cors = require("cors");
 const express = require('express');
@@ -6,7 +7,9 @@ const app = express()
 connection();
 
 app.use(express.json())
-app.use(cors())
+app.use(cors());
+
+app.use("/api/tasks", tasks)
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log('Listening on port ${port}...'));
